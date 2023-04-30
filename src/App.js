94fa4a07f87e564d70360a8caf333430abbb2ec5
Barakat Bolaji <barakatbolaji65@gmail.com>
2023-04-30 14:18:55 +0100

    - implement and decoration of the TopButtons - importation of icons -implement the input field and styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import "./App.css";
 import UilReact from "@iconscout/react-unicons/icons/uil-react";
+import TopButtons from "./components/TopButtons";
+import Input from "./components/Input";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-slate-600 to-gray-300 h-fit shadow-xl shadow-cyan-50">
+      <TopButtons />
+      <Input />
+    </div>
+  );
 }
 
 export default App;
