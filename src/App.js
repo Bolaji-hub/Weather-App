ac613a9671087fde24dcba25895f324206e3a427
Barakat Bolaji <barakatbolaji65@gmail.com>
2023-04-30 21:36:20 +0100

    - implement and decorate the temperature and detail section

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import UilReact from "@iconscout/react-unicons/icons/uil-react";
 import TopButtons from "./components/TopButtons";
 import Input from "./components/Input";
 import TimeAndLocation from "./components/TimeAndLocation";
+import TemperatureAndDetails from "./components/TemperatureAndDetails";
 
 function App() {
   return (
-    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-slate-600 to-gray-300 h-fit shadow-xl shadow-cyan-50">
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-cyan-50">
       <TopButtons />
       <Input />
       <TimeAndLocation />
+      <TemperatureAndDetails />
     </div>
   );
 }
